a090f393e109d26456da908f04a4c10f3ac76624
MOVZX <movzx@yahoo.com>
2017-09-02 02:03:18 +0300

    ABCDEFGHIJKLMN
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,10 +46,8 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 ramoops_memreserve=4M boot_cpus=0-3,4-5 no_console_suspend
-# Audio Mods
-BOARD_KERNEL_CMDLINE += snd-soc-msm8x16-wcd.high_perf_mode=1
 # SELinux Mode
-# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 # Darkness Mode
 BOARD_KERNEL_CMDLINE += bobs_and_vegena=1
 BOARD_KERNEL_BASE := 0x80000000
